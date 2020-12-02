8ccfb9fd775500a5bef4ad2ad4e8461383ad21ba
CaptainShepard01 <BalAnton01@users.noreply.github.com>
2020-12-02 22:14:07 +0200

    Lab_7 (RK matrix search), Lab_8 (Substring searching algorithms)

--- Algorithms_lab_7/Algorithms_lab_7.cpp
+++ Algorithms_lab_7/Algorithms_lab_7.cpp
@@ -5,74 +5,86 @@
 #include <stdio.h>
 #include <cmath>
 
-int RK_search(const std::string& str, const std::string& substr) { 	// Алгоритм Рабіна-Карпа
-	const int p = 31;
-	std::vector<long long> p_pow(std::max(str.length(), substr.length()));
-	p_pow[0] = 1;
-	for (size_t i = 1; i < p_pow.size(); ++i)
-		p_pow[i] = p_pow[i - 1] * p;
+int RK_search(const std::string& str, const std::string& substr) {
+    const int p = 31;
+    static std::vector<long long> p_pow(std::max(str.length(), substr.length()));
+    p_pow[0] = 1;
+    for (size_t i = 1; i < p_pow.size(); i++)
+        p_pow[i] = p_pow[i - 1] * p;
 
-	std::vector<long long> h(str.length());
-	for (size_t i = 0; i < str.length(); ++i)
-	{
-		h[i] = (str[i] - 'a' + 1) * p_pow[i];
-		if (i)  h[i] += h[i - 1];
-	}
+    std::vector<long long> h(str.length());
+    for (size_t i = 0; i < str.length(); i++) {
+        h[i] = (str[i] - 'a' + 1) * p_pow[i];
+        if (i)
+            h[i] += h[i - 1];
+    }
 
-	long long h_s = 0;
-	for (size_t i = 0; i < substr.length(); ++i)
-		h_s += (substr[i] - 'a' + 1) * p_pow[i];
+    long long h_s = 0;
+    for (size_t i = 0; i < substr.length(); i++)
+        h_s += (substr[i] - 'a' + 1) * p_pow[i];
 
-	for (size_t i = 0; i + substr.length() - 1 < str.length(); ++i)
-	{
-		long long cur_h = h[i + substr.length() - 1];
-		if (i)  cur_h -= h[i - 1];
-		if (cur_h == h_s * p_pow[i])
-			return i;
-	}
+    for (size_t i = 0; i + substr.length() - 1 < str.length(); i++) {
+        long long cur_h = h[i + substr.length() - 1];
+        if (i)
+            cur_h -= h[i - 1];
+        if (cur_h == h_s * p_pow[i])
+            return i;
+    }
 
-	return -1;
+    return -1;
 }
 
-std::pair<int, int> RK_matrix(const std::vector<std::string>& text, const std::vector<std::string>& pattern) { // Узагальнений алгоритм Рабіна-Карпа
-	int m = text.size();
-	int n = pattern.size();
+std::pair<int, int> RK_matrix(const std::vector<std::string>& text, const std::vector<std::string>& pattern) {
+    int m = text.size();
+    int n = pattern.size();
 
-	for (int row = 0; row < m - n + 1; row++) {
+    for (int row = 0; row < m - n + 1; row++) {
 
-		int firstEntryCol = -1;
-		int prevCol = -1;
+        int firstEntryCol = -1;
+        int prevCol = -1;
 
-		do {
-			prevCol = firstEntryCol;
-			firstEntryCol = RK_search(text[row].substr(prevCol + 1, n), pattern[0]);
+        do {
+            prevCol = firstEntryCol;
+            firstEntryCol = RK_search(text[row].substr(prevCol + 1), pattern[0]);
 
-			if (firstEntryCol >= 0) {
+            if (firstEntryCol >= 0) {
 
-				firstEntryCol += prevCol;
+                firstEntryCol += prevCol + 1;
 
-				bool entry = true;
+                bool entry = true;
 
-				for (int j = 0; j < n; j++) {
-					if (RK_search(text[row + j].substr(firstEntryCol, n), pattern[j]) != 0) {
-						entry = false;
-						break;
-					}
-				}
+                for (int j = 1; j < n; j++) {
+                    if (RK_search(text[row + j].substr(firstEntryCol, n), pattern[j]) != 0) {
+                        entry = false;
+                        break;
+                    }
+                }
 
-				if (entry)
-					return { row, firstEntryCol };
-			}
+                if (entry)
+                    return { row, firstEntryCol };
+            }
 
-		} while (firstEntryCol != -1);
+        } while (firstEntryCol != -1);
 
-	}
+    }
 
-	return { -1, -1 };
-
+    return { -1, -1 };
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    std::vector<std::string> text = { "hello",
+                      "hears",
+                      "nwill",
+                      "cloud",
+                      "cutie" };
+
+    std::vector<std::string> pattern = { "hell",
+                       "ear",
+                       "will" };
+
+    auto pair = RK_matrix(text, pattern);
+
+    std::cout << pair.first << ' ' << pair.second << std::endl;
+    //std::cout << RK_search("olol", "lol");
 }
+
--- Algorithms_lab_8/Algorithms_lab_8.cpp
+++ Algorithms_lab_8/Algorithms_lab_8.cpp
@@ -1,133 +1,239 @@
-﻿#include <stdio.h>
-#include <iostream>
+﻿#include <string>
 #include <vector>
-#include <string>
+#include <iostream>
+#include <cmath>
 
+#define ALPHABET 256
 
-int naive_Search(const std::string& str, const std::string& substr) { // наївний алгоритм
-	int m = str.length();
-	int n = substr.length();
+template <class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) {
+	os << '[';
+	for (auto i = v.begin(); i != v.end(); ++i)
+		os << ' ' << *i;
+
+	os << " ]";
+	return os;
+}
+
+std::vector<int> naiveSearch(const std::string& pattern, const std::string& text) {
+	std::vector<int> result;
+
+	int m = text.length();
+	int n = pattern.length();
 	for (int i = 0; i <= m - n; i++) {
 		bool flag = true;
 		for (int j = 0; j < n; j++) {
-			if (str[i + j] != substr[j]) {
+			if (text[i + j] != pattern[j]) {
 				flag = false;
 				break;
 			}
 		}
-		if (flag) return i;
+		if (flag)
+			result.push_back(i++);
 	}
-	return -1;
+	return result;
 }
 
-int RK_search(const std::string& str, const std::string& substr) { 	// Алгоритм Рабіна-Карпа
-	const int p = 31;
-	std::vector<long long> p_pow(std::max(str.length(), substr.length()));
-	p_pow[0] = 1;
-	for (size_t i = 1; i < p_pow.size(); ++i)
-		p_pow[i] = p_pow[i - 1] * p;
+std::vector<int> RKSearch(const std::string& pattern, const std::string& text, int primeNumber = 31) {
+	std::vector<int> result;
 
-	std::vector<long long> h(str.length());
-	for (size_t i = 0; i < str.length(); ++i)
-	{
-		h[i] = (str[i] - 'a' + 1) * p_pow[i];
-		if (i)  h[i] += h[i - 1];
+	int patternLenght = pattern.length();
+	int textLenght = text.length();
+	int i = 0, j = 0;
+	int patternHash = 0;
+	int textHash = 0;
+	int h = (int)pow(ALPHABET, patternLenght - 1) % primeNumber;
+
+	for (i = 0; i < patternLenght; i++) {
+		patternHash = (ALPHABET * patternHash + pattern[i]) % primeNumber;
+		textHash = (ALPHABET * textHash + text[i]) % primeNumber;
 	}
 
-	long long h_s = 0;
-	for (size_t i = 0; i < substr.length(); ++i)
-		h_s += (substr[i] - 'a' + 1) * p_pow[i];
+	for (i = 0; i <= textLenght - patternLenght; i++) {
+		if (patternHash == textHash) {
+			for (j = 0; j < patternLenght; j++) {
+				if (text[i + j] != pattern[j])
+					break;
+			}
 
-	for (size_t i = 0; i + substr.length() - 1 < str.length(); ++i)
-	{
-		long long cur_h = h[i + substr.length() - 1];
-		if (i)  cur_h -= h[i - 1];
-		if (cur_h == h_s * p_pow[i])
-			return i;
-	}
+			if (j == patternLenght)
+				//	std::cout << "Pattern found at index " << i << std::endl;
+				result.push_back(i);
+		}
 
-	return -1;
-}
+		if (i < textLenght - patternLenght) {
+			textHash = (ALPHABET * (textHash - text[i] * h) + text[i + patternLenght]) % primeNumber;
 
-int Horsool_search(const std::string& str, const std::string& substr) { // Алгоритм Хорспула
-	unsigned int skip_table[ALPHABET];
-	int i;
-
-	for (i = 0; i < ALPHABET; i++)
-		skip_table[i] = substr.size();
-
-	for (i = 0; i < substr.size() - 1; i++)
-		skip_table[(int)what[i]] = substr.size() - i - 1;
-
-	i = 0;
-	while (i <= str.size() - substr.size()) {
-		if (str[i + substr.size() - 1] == substr[substr.size() - 1])
-			if (compare(str.begin() + i, substr.begin(), substr.size() - 2) == true)
-				return i;
-
-		i += skip_table[(int)str[i + substr.size() - 1]];
-	}
-
-	return -1;
-}
-
-int BM_search(const std::string& str, const std::string& substr) { 	// Алгоритм Боєра-Мура
-	std::vector<int> good_shift(substr.size() + 1, substr.size());
-	std::vector<int> bad_char(ALPHABET, substr.size());
-
-	std::vector<int> z(substr.size(), 0);
-	for (int j = 1, maxZidx = 0, maxZ = 0; j < substr.size(); ++j) {
-		if (j <= maxZ) z[j] = std::min(maxZ - j + 1, z[j - maxZidx]);
-		while (j + z[j] < substr.size() && str[substr.size() - 1 - z[j]] == str[substr.size() - 1 - (j + z[j])]) z[j]++;
-		if (j + z[j] - 1 > maxZ) {
-			maxZidx = j;
-			maxZ = j + z[j] - 1;
+			if (textHash < 0)
+				textHash = (textHash + primeNumber);
 		}
 	}
-	for (int j = substr.size() - 1; j > 0; j--) good_shift[substr.size() - z[j]] = j; //цикл №1
-	for (int j = 1, r = 0; j <= substr.size() - 1; j++) //цикл №2
-		if (j + z[j] == substr.size())
-			for (; r <= j; r++)
-				if (good_shift[r] == substr.size()) good_shift[r] = j;
+	return result;
+}
 
-	for (int j = 0; j < substr.size() - 1; j++) bad_char[(int)substr[j]] = substr.size() - j - 1;
+std::vector<int> prefixFunction(std::string s) {
+	int n = (int)s.length();
+	std::vector<int> pi(n, 0);
+
+	for (int i = 1; i < n; i++) {
+		int j = pi[i - 1];
+
+		while (j > 0 && s[i] != s[j])
+			j = pi[j - 1];
+
+		if (s[i] == s[j])
+			j++;
+
+		pi[i] = j;
+	}
+	return pi;
+}
+
+std::vector<int> KMPSearch(const std::string& pattern, const std::string& text) {
+	std::vector<int> result;
+
+	int patternLength = pattern.length();
+	int textLength = text.length();
+
+	auto prefixes = prefixFunction(pattern);
 
 	int i = 0;
-	while (i <= substr.size() - str.size()) {
-		int j = 0;
-		for (j = substr.size() - 1; j >= 0 && substr[j] == str[i + j]; --j);
-		if (j < 0) {
-			return i;
+	int j = 0;
+
+	while (i < textLength) {
+		if (pattern[j] == text[i]) {
+			j++;
+			i++;
 		}
-		else i += std::max((good_shift[j + 1]), (int)(bad_char[(int)str[i + j]] - substr.size() + j + 1));
+
+		if (j == patternLength) {
+			//	std::cout << "Found pattern at index " << i - j << std::endl;
+			result.push_back(i - j);
+
+			j = prefixes[j - 1];
+		}
+
+		else if (i < textLength && pattern[j] != text[i]) {
+			if (j != 0)
+				j = prefixes[j - 1];
+			else
+				i = i + 1;
+		}
 	}
-	return -1;
+
+	return result;
 }
 
-int KMP_search(const std::string& str, const std::string& substr) {	// Алгоритм Кнута-Моріса-Прата
-	std::vector<int> z;
-	z.resize(substr.size());
-	z[0] = 0;
-	for (int i = 1; i < z.size(); ++i) {
-		int pos = z[i - 1];
-		while (pos > 0 && substr[pos] != substr[i])
-			pos = z[pos - 1];
-		z[i] = pos + (substr[pos] == substr[i] ? 1 : 0);
+void preprocessStrongSuffix(std::vector<int>& shift, std::vector<int>& bpos, std::string pattern) {
+	int m = pattern.length();
+	int i = m, j = m + 1;
+
+	bpos[i] = j;
+	while (i > 0) {
+		while (j <= m && pattern[i - 1] != pattern[j - 1]) {
+			if (shift[j] == 0)
+				shift[j] = j - i;
+			j = bpos[j];
+		}
+		i--;
+		j--;
+		bpos[i] = j;
 	}
 
-	int pos = 0;
-	for (int i = 0; i < str.size(); ++i) {
-		while (pos > 0 && (pos >= substr.size() || substr[pos] != str[i]))
-			pos = z[pos - 1];
-		if (str[i] == substr[pos]) pos++;
-		if (pos == substr.size())
-			return (i - pos + 1);
+	m = pattern.length();
+
+	i = 0, j = 0;
+
+	j = bpos[0];
+
+	for (i = 0; i <= m; i++) {
+		if (shift[i] == 0)
+			shift[i] = j;
+
+		if (i == j)
+			j = bpos[j];
 	}
-	return -1;
 }
 
+std::vector<int> badCharHeuristic(const std::string& str) {
+	int i = 0;
+	int size = str.length();
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	std::vector<int> badchar(ALPHABET, -1);
+
+	for (i = 0; i < size; i++)
+		badchar[(int)str[i]] = i;
+
+	return badchar;
+}
+
+std::vector<int> BMSearch(const std::string& pattern, const std::string& text) {
+	int m = pattern.size();
+	int n = text.size();
+
+	std::vector<int>bpos(m + 1), shift(m + 1, 0), result;
+
+	std::vector<int> badchar = badCharHeuristic(pattern);
+
+	preprocessStrongSuffix(shift, bpos, pattern);
+
+	int s = 0;
+
+	int d_1 = 0;
+
+	while (s <= (n - m)) {
+		int j = m - 1;
+
+		while (j >= 0 && pattern[j] == text[s + j])
+			j--;
+
+		if (j < 0) {
+			//	std::cout << "Pattern occurs at shift = " << s << std::endl;
+			result.push_back(s);
+			d_1 = s + (s + m < n) ? m - badchar[text[s + m]] : 1;
+			s += std::max(shift[0], d_1);
+		}
+
+		else
+			s += std::max(1, j - badchar[text[s + j]]);
+	}
+
+	return result;
+}
+
+std::vector<int> shiftTable(const std::string& p) {
+	std::vector<int> table(ALPHABET, p.length());
+
+	for (int j = 0; j < p.length() - 1; j++) {
+		table[(int)p[j]] = p.length() - 1 - j;
+	}
+	return table;
+}
+
+std::vector<int> HSearch(const std::string& pattern, const std::string& text) {
+	std::vector<int> table = shiftTable(pattern), result;
+
+	int k = 0, m = pattern.length(), n = text.length();
+
+	int i = m - 1;
+	while (i < n) {
+		k = 0;
+		while (k < m && pattern[m - 1 - k] == text[i - k])
+			k++;
+		if (k == m) {
+			result.push_back(i - m + 1);
+			i++;
+		}
+		else
+			i += table[text[i]];
+	}
+	return result;
+}
+
+int main() {
+	std::cout << naiveSearch("abcd", "abcdababcdabcdcdabcddddcaabcd") << std::endl;
+	std::cout << RKSearch("abcd", "abcdababcdabcdcdabcddddcaabcd") << std::endl;
+	std::cout << KMPSearch("abcd", "abcdababcdabcdcdabcddddcaabcd") << std::endl;
+	std::cout << BMSearch("abcd", "abcdababcdabcdcdabcddddcaabcd") << std::endl;
+	std::cout << HSearch("abcd", "abcdababcdabcdcdabcddddcaabcd") << std::endl;
 }
